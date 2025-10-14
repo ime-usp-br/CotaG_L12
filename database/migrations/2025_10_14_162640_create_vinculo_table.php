11997956155ff5288bcb70b49f39fec2dfc90ebe
Joao Avella <joao@Svapin-Dev2.ime.usp.br>
2025-10-14 17:10:08 -0300

    feat(database): Cria migrations para as tabelas de domínio
    Cria as migrations para as seguintes tabelas:
    - pessoa
    - vinculo
    - cota
    - cota_especial
    - lancamento
    
    Essas tabelas representam as entidades centrais do sistema CotaG.
    
    Ref: #17

---
+++ database/migrations/2025_10_14_162640_create_vinculo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vinculo', function (Blueprint $table) {
+            $table->integer('codpes')->primary();
+            $table->foreign('codpes')
+                  ->references('codpes')
+                  ->on('pessoa')
+                  ->onDelete('cascade');
+            $table->string('vinculo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vinculo');
+    }
+};
---
+++ database/migrations/2025_10_14_164147_create_cota_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cota', function (Blueprint $table) {
+            $table->id();
+            $table->integer('valor');
+            $table->string('vinculo');
+            $table->foreign('vinculo')
+                  ->references('vinculo')
+                  ->on('vinculo')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cota');
+    }
+};
