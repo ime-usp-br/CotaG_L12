a90790582300e1ccb610d64399eef4a685549cdf
Joao Avella <joao@Svapin-Dev2.ime.usp.br>
2025-10-15 14:51:10 -0300

    feat(database): Cria migrations para as tabelas de organização interna
    Cria as migrations para as seguintes tabelas:
    - ou (unidades organizacionais)
    - grupo
    - usuario_grupo (tabela pivot)
    
    Essas tabelas são usadas para organizar os usuários em grupos e unidades organizacionais, permitindo um controle de acesso mais granular.
    
    Ref: #18

---
+++ database/migrations/2025_10_15_144145_create_usuario_grupo_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuario_grupo', function (Blueprint $table) {
+            $table->integer('usuario_id');
+            $table->foreign('usuario_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->integer('grupo_id');
+            $table->foreign('grupo_id')
+                  ->references('id')
+                  ->on('grupo')
+                  ->onDelete('cascade');
+            $table->primary(['usuario_id','grupo_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario_grupo');
+    }
+};
